291ef7eef46158dfa0d1e46815340db9f602a84c
Papa Ba GAYE <83582380+PapaBaGAYE@users.noreply.github.com>
2022-06-21 11:49:15 +0000

    Add files via upload

---
+++ Capteur_Ultrason.ino
@@ -0,0 +1,14 @@
+#include <HCSR04.h>
+
+HCSR04 hc(2, 3); //initialisation class HCSR04 (trig pin , echo pin)
+
+void setup()
+{
+    Serial.begin(9600);
+}
+
+void loop()
+{
+    Serial.println(hc.dist()); // return curent distance in serial
+    delay(1000);                 // we suggest to use over 60ms measurement cycle, in order to prevent trigger signal to the echo signal.
+}
