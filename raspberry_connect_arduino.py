2faf3a8ac1796c56426b445e605d1e53d4904141
Papa Ba GAYE <83582380+PapaBaGAYE@users.noreply.github.com>
2022-08-12 10:22:22 +0000

    Mise à jour .py

--- raspberry_connect_arduino.py
+++ raspberry_connect_arduino.py
@@ -3,7 +3,7 @@ import serial
 ser = serial.Serial('/dev/ttyUSB0',9600, timeout=1 )
 
 while True:
-    with open ("pape.txt", "r") as f:
+    with open ("file.txt", "r") as f:
         g = f.readlines()[-1]
     ser.write(g.encode())
     while (ser.in_waiting <= 0):
@@ -14,6 +14,6 @@ while True:
 
     print(b)
 
-    file = open("serie_Ard_vers_pi.txt","w")
+    file = open("Ard_to_rasp.txt","w")
     file.write(a)
     file.close()
